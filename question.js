2d64b0ad81c1c6a06daa236a9d4ea66f60ba9dca
Cristian Ramirez <cristiand92376@gmail.com>
2020-06-07 23:01:27 -0700

    finished most of the quiz just needed the a way to save the high score

---
+++ question.js
@@ -0,0 +1,30 @@
+
+
+//array of objects
+const quiz = [
+    {
+        q:'What does HTML stand for?',
+        options:['Hey There Man Looking', 'Hi There MandaLorian', 'Hyperext Markup Language', 'Nothing'],
+        answer:2
+    },
+    {
+        q:'this is not a good question?',
+        options:['True','False'],
+        answer:0
+    },
+    {
+        q:'How many month are in a year?',
+        options:['1','5', '9','12'],
+        answer:3
+    },
+    {
+        q:'6 + 5 = 65?',
+        options:['True','False'],
+        answer:1
+    },
+    {
+        q:'What are the three fundamental languages in codding?',
+        options:['red, blue, and green','hamburger,soda, and fries','1,2,and3','html,css,and javascript'],
+        answer:3
+    }
+]
